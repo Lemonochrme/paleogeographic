a4d3c940f4d6b5e0f4f93563b6d3a799956db567
Lemonochrme <robin.marn.pro@gmail.com>
2025-03-25 16:12:41 +0100

    Update README.md with dataset links and modify salinity filtering in map visualization

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # paleogeographic
 Python scripts to visualize map from paliogeographic dataset 
 
-Dataset used : https://www.earthbyte.org/webdav/ftp/Data_Collections/Scotese_Wright_2018_PaleoDEM/Scotese_Wright_2018_Maps_1-88_1degX1deg_PaleoDEMS_nc.zip
+Paleogeography dataset : 
+https://www.earthbyte.org/webdav/ftp/Data_Collections/Scotese_Wright_2018_PaleoDEM/Scotese_Wright_2018_Maps_1-88_1degX1deg_PaleoDEMS_nc.zip
+
+Water salinity dataset :
+https://www.ncei.noaa.gov/access/world-ocean-atlas-2023/bin/woa23.pl
--- WOA/map.py
+++ WOA/map.py
@@ -12,12 +12,22 @@ salinity = data['s_an'] # s_an variable represent climatological mean salinity
 
 depth_index = 0  # (0, 5, 10, ..., 5000m)
 
-salinity_filtered = salinity.where(salinity >= 20)
+salinity_filtered = salinity.where(salinity >= 32)
+
+# plt.figure(figsize=(16, 9))
+# sns.heatmap(salinity_filtered[0, depth_index, ::-1, :], cmap='inferno_r', cbar_kws={'label': 'Salinity'}, 
+#             xticklabels=10, yticklabels=10)
+# plt.title('Mean Water Salinity at 0 m Depth')
+# plt.xlabel('Longitude')
+# plt.ylabel('Latitude')
+# plt.show()
 
 plt.figure(figsize=(16, 9))
-sns.heatmap(salinity_filtered[0, depth_index, ::-1, :], cmap='inferno_r', cbar_kws={'label': 'Salinity'}, 
-            xticklabels=10, yticklabels=10)
-plt.title('Mean Water Salinity at 0 m Depth')
+# contour = plt.contour(salinity_filtered[0, depth_index, :, :], levels=42, colors='grey', linewidths=0.5)
+# plt.clabel(contour, inline=True, fontsize=8, fmt='%1.1f')
+contourf = plt.contourf(salinity_filtered[0, depth_index, :, :], levels=20, cmap='RdYlBu')
+plt.colorbar(contourf, label='Salinity')
+plt.title('2023 Mean Water Salinity at 0 m Depth')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 plt.show()
